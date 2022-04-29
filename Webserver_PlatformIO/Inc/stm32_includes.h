00eaadfe83a5c8979868fd2457d084eb2ac935ff
stijnvandenbossche <61984433+stijnvandenbossche@users.noreply.github.com>
2022-04-29 12:53:25 +0200

    moved includes to separate file

---
+++ Webserver_PlatformIO/Inc/stm32_includes.h
@@ -0,0 +1,20 @@
+/*includes for stm32 version*/
+
+#include "crc.h"
+#include "dma2d.h"
+#include "fatfs.h"
+#include "i2c.h"
+#include "ltdc.h"
+#include "lwip.h"
+#include "sdmmc.h"
+#include "usart.h"
+#include "gpio.h"
+#include "fmc.h"
+
+#include "init.h"
+#include "httpd.h"
+
+#include "stm32746g_discovery.h"
+#include "stm32746g_discovery_sdram.h"
+#include "stm32746g_discovery_ts.h"
+#include "stm32746g_discovery_lcd.h"
